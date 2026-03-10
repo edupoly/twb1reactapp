869b768d2b6fa0257497396d5ef38db6abe6dbde
edupoly <info.edupoly@gmail.com>
2026-03-10 10:08:17 +0530

    more depth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Counter from "./Counter";
-import Todolist from "./Todolist";
 function App() {
   return (
     <div className="mybox">
       <h1 className="bg-dark text-info">
         Welcome to Enterprise Level ReactJS Application Development
       </h1>
-      <Counter></Counter>
-      <Todolist></Todolist>
     </div>
   );
 }
