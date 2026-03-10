d33ef788ccf2727186f9a438f6f9d0a09da20bb2
edupoly <info.edupoly@gmail.com>
2026-03-10 11:51:45 +0530

    useEffect use

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect(() => {
+    console.log("useEffect on mounting");
+  }, []);
+  useEffect(() => {
+    console.log("useEffect without dependency array");
+  });
+  useEffect(() => {
+    console.log("x updated");
+  }, [x]);
+  useEffect(() => {
+    console.log("y updated");
+  }, [y]);
   return (
     <div className="mybox">
       <h1 className="bg-dark text-info">
         Welcome to Enterprise Level ReactJS Application Development
       </h1>
+      <h1>X Count:{x}</h1>
+      <button
+        onClick={() => {
+          setX(x + 4);
+        }}
+      >
+        Inc
+      </button>
+      <h1>Y Count:{y}</h1>
+      <button
+        onClick={() => {
+          setY(y + 5);
+        }}
+      >
+        Inc
+      </button>
     </div>
   );
 }
